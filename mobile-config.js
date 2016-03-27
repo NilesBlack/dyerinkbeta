ebdfd3ce881e710286bfbb3d689760f02cca8acb
Niles Black <nilesdark@gmail.com>
2016-03-27 06:38:33 -0400

    Added the appropriate icons and splash screens for Android build

---
+++ mobile-config.js
@@ -0,0 +1,34 @@
+App.info({
+  name: 'Dyer Ink',
+  description: 'Dyer Ink - a home for the creative transcendent',
+  author: 'Black OSD',
+  email: 'contact@dyerink.com',
+  website: 'http://dyerink.com',
+  version: '0.0.1'
+});
+
+App.icons({
+    
+  // Android
+  'android_mdpi': 'resources/icons/icon-48x48.png',
+  'android_hdpi': 'resources/icons/icon-72x72.png',
+  'android_xhdpi': 'resources/icons/icon-96x96.png',
+  'android_xxhdpi': 'resources/icons/icon-144x144.png',
+  'android_xxxhdpi': 'resources/icons/icon-192x192.png'
+});
+
+App.launchScreens({
+    
+  // Android
+  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+});
+
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#006994');
